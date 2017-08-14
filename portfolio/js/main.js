718dce22037e7e8a6d9176a37043b83344b2f766
Diana Hernandez <DianaHernandezJ@gmail.com>
2017-08-13 23:48:51 -0400

    Pseudo Code

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,1 +1,4 @@
-// Make it rain!
+//When user scrolls down to about me section
+  //Navigation bar is no longer transparent and becomes sticky
+    //Navigation links move to the right of the navigation bar
+      //And my name appears on the left side of the navigation bar
